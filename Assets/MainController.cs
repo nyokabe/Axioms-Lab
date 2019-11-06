a3557b58efe5e49ee4755e542026e4745f85ac0b
Hellfire <holdenelling@holdens-MacBook-Pro.local>
2019-11-06 16:53:49 -0500

    update

---
+++ Assets/MainController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour
+{
+    public bool ballDrop;
+    public bool ballRoll;
+    public bool cannon;
+    public bool newton;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballDrop = true;
+        ballRoll = false;
+        newton = false;
+        cannon = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Transitions()
+    {
+    }
+}
