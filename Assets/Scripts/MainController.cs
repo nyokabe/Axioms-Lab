2ab3002325c0034cc7f5eb3d2c7665d192137d38
Hellfire <holdenelling@holdens-MacBook-Pro.local>
2019-11-10 17:13:34 -0500

    contr

--- Assets/Scripts/MainController.cs
+++ Assets/Scripts/MainController.cs
@@ -8,7 +8,18 @@ public class MainController : MonoBehaviour
     public bool ballRoll;
     public bool cannon;
     public bool newton;
-    
+    public GameObject levelBallDrop;
+    public GameObject levelBallRoll;
+    public GameObject levelCannon;
+    public GameObject levelNewton;
+
+    AudioSource Startmusic;
+    AudioSource LevelMusic1;
+    AudioSource LevelMusic2;
+    AudioSource TransMusic;
+    AudioSource WinMusic;
+    AudioSource ProfIntro;
+    AudioSource ProfTalk1;
 
     // Start is called before the first frame update
     void Start()
@@ -17,15 +28,69 @@ public class MainController : MonoBehaviour
         ballRoll = false;
         newton = false;
         cannon = false;
+        Startmusic = GetComponent<AudioSource>();
+        LevelMusic1 = GetComponent<AudioSource>();
+        LevelMusic2 = GetComponent<AudioSource>();
+        TransMusic = GetComponent<AudioSource>();
+        WinMusic = GetComponent<AudioSource>();
+        ProfIntro = GetComponent<AudioSource>();
+        ProfTalk1 = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       Transitions(); 
     }
 
     void Transitions()
     {
+        if (ballDrop = true)
+        {
+            levelBallRoll.SetActive(false);
+            levelCannon.SetActive(false);
+            levelNewton.SetActive(false);
+            levelBallDrop.SetActive(true);
+        }
+        if (ballRoll = true)
+        {
+            levelCannon.SetActive(false);
+            levelNewton.SetActive(false);
+            levelBallDrop.SetActive(false);
+            levelBallRoll.SetActive(true);
+        }
+        if (newton = true)
+        {
+            levelBallRoll.SetActive(false);
+            levelCannon.SetActive(false);
+            levelBallDrop.SetActive(false);
+            levelNewton.SetActive(true);
+        }
+        if (cannon = true)
+        {
+            levelBallRoll.SetActive(false);
+            levelNewton.SetActive(false);
+            levelBallDrop.SetActive(false);
+            levelCannon.SetActive(true);
+        }
+    }
+    Music()
+    {
+        if (cannon = true)
+        {
+
+        }
+        if (newton = true)
+        {
+
+        }
+        if (ballRoll = true)
+        {
+
+        }
+        if (ballDrop = true)
+        {
+
+        }
     }
 }
